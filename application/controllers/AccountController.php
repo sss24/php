99453d9a07ced891ab3b11feb8aa50b09a5fb52f
Yurkab <yurkab24@gmail.com>
2018-12-22 17:15:21 +0300

    add new file class

---
+++ application/controllers/AccountController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace application\controllers;
+
+class AccountController {
+
+    public function loginAction() {
+        echo '<br>login';
+    }
+
+    public function registAction() {
+        echo '<br>regist';
+    }
+
+}
---
+++ application/controllers/MainController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace application\controllers;
+
+class MainController {
+
+    public function indexAction() {
+        echo '<br>index';
+    }
+}
