55aec2698a7d76d4ee58357546f18339f18bb802
Yurkab <yurkab24@gmail.com>
2018-12-22 16:28:10 +0300

    add method match() Route.php

--- application/core/Router.php
+++ application/core/Router.php
@@ -10,22 +10,33 @@ class Router
     public function __construct()
     {
         $arr = require_once 'application/config/routes.php';
-        debug($arr);
+        foreach ($arr as $kay => $value) {
+            $this->add($kay, $value);
+        }
+
     }
 
-    public function add()
+    public function add($route, $params)
     {
-
+        $route =  '#^' . $route . '$#';
+        $this->routes[$route] = $params;
     }
 
     public function match()
     {
-
+        $url = trim($_SERVER['REQUEST_URI'], '/');
+        foreach ( $this->routes as $route => $params) {
+            if(preg_match($route, $url, $matches)) {
+                $this->params = $params;
+                return true;
+            }
+        }
+        return false;
     }
 
     public function run()
     {
-        echo 'start method run()';
+
     }
 
 
